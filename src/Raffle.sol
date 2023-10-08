3e81d72423c7b584a1f7aed81c63ebbaf4c0975f
Przemo <przemo06133@gmail.com>
2023-10-08 21:05:13 +0200

    Add VRFCoordinatorV2Interface

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /**
  * @title A sampleRaffle Contract
  * @author Przemo
@@ -11,15 +13,38 @@ pragma solidity ^0.8.18;
 contract Raffle {
     error Raffle__NotEnoughtEthSent();
 
+    uint16 private constant REQ_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
     uint256 private immutable i_enterenceFee;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subId;
+    uint32 private immutable i_callbackGasLimit;
 
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
     /** Events */
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 enterenceFee) {
+    constructor(
+        uint256 enterenceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subId,
+        uint32 callbackGasLimit
+    ) {
         i_enterenceFee = enterenceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subId = subId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimeStamp - block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -31,7 +56,24 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        // 1. Get random number
+        // 2. Use the random number to pcik a player
+        // 3. Be autimatically called
+
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        // Will revert if subscription is not set and funded.
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gas lane
+            i_subId,
+            REQ_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
 
     /** Getter Function */
     function getEnteranceFee() external view returns (uint256) {
