dbc145a9e83918f37b637b395634b6ae3077cfe4
Przemo <przemo06133@gmail.com>
2023-10-08 19:07:23 +0200

    Add event and emit

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,7 +9,32 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
-    function enterRaffle() public {}
+    error Raffle__NotEnoughtEthSent();
+
+    uint256 private immutable i_enterenceFee;
+
+    address payable[] private s_players;
+
+    /** Events */
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 enterenceFee) {
+        i_enterenceFee = enterenceFee;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_enterenceFee, "Not enough ETH sent!");
+        if (msg.value < i_enterenceFee) {
+            revert Raffle__NotEnoughtEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
 
     function pickWinner() public {}
+
+    /** Getter Function */
+    function getEnteranceFee() external view returns (uint256) {
+        return i_enterenceFee;
+    }
 }
