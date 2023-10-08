c692ac0d097c3f082267f88efa8f6398a0fc7ce2
Przemo <przemo06133@gmail.com>
2023-10-08 15:51:46 +0200

    Cleanup, Init Raffle contract

---
+++ src/Raffle.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title A sampleRaffle Contract
+ * @author Przemo
+ * @notice Create a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+}
