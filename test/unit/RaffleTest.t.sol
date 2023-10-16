436f09fbf96e16c3f4a6f8a95a5d955f38363689
Przemo <przemo06133@gmail.com>
2023-10-16 20:31:12 +0200

    Add test cases for checkUpKeep

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -90,4 +90,34 @@ contract RaffleTest is Test {
         vm.prank(PLAYER);
         raffle.enterRaffle{value: enterenceFee}();
     }
+
+    /** Test checkUpKeep */
+    function testCheckUpKeepReturnsFalseIfNoBalance() public {
+        // Arrange
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        // console.log("", raffle.)
+
+        // Assert
+        assert(!upkeepNeeded);
+    }
+
+    function testCheckUpKeepReturnsFalseIfRaffleNotOpen() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: enterenceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        // Assert
+        assert(upkeepNeeded == false);
+    }
 }
